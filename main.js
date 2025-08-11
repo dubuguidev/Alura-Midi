572d1a4e89f722e7b9cbd1757f3c3fd0dc4a05a6
Dubugui <gabrielbcostadev@gmail.com>
2025-08-11 11:53:02 -0300

    primeiro dia

---
+++ main.js
@@ -0,0 +1,53 @@
+function tocaSomBolinhadeGolf () {
+    document.querySelector('#som_tecla_bolinhadegolf').play();
+}
+
+document.querySelector('.tecla_bolinhadegolf').onclick = tocaSomBolinhadeGolf;
+
+function tocaBrazino () {
+    document.querySelector('#som_tecla_brazino').play();
+}
+
+document.querySelector('.tecla_brazino').onclick = tocaBrazino;
+
+function tocaCatlaught () {
+    document.querySelector('#som_tecla_catlaught').play();
+}
+
+document.querySelector('.tecla_catlaught').onclick = tocaCatlaught;
+
+function tocaCatsad () {
+    document.querySelector('#som_tecla_catsad').play();
+}
+
+document.querySelector('.tecla_catsad').onclick = tocaCatsad;
+
+function tocaBuzina () {
+    document.querySelector('#som_tecla_dilera').play();
+}
+
+document.querySelector('.tecla_dilera').onclick = tocaBuzina;
+
+function tocaFazoL () {
+    document.querySelector('#som_tecla_fazoL').play();
+}
+
+document.querySelector('.tecla_fazoL').onclick = tocaFazoL;
+
+function tocaOruam () {
+    document.querySelector('#som_tecla_oruam').play();
+}
+
+document.querySelector('.tecla_oruam').onclick = tocaOruam;
+
+function tocaTecnologia () {
+    document.querySelector('#som_tecla_tecnologia').play();
+}
+
+document.querySelector('.tecla_tecnologia').onclick = tocaTecnologia;
+
+function tocaXaropinho () {
+    document.querySelector('#som_tecla_xaropinho').play();
+}
+
+document.querySelector('.tecla_xaropinho').onclick = tocaXaropinho;
